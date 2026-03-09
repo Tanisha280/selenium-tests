afd19b1cbf79b54552175a1e602a709fab111711
tanisha <tanishakalokhe858@gmail.com>
2026-03-09 20:57:03 +0530

    Remove target folder

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver",
+        "C:\\Users\\TANISHA KALOKHE\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+
+        System.out.println(driver.getTitle());
+
+        driver.quit();
+    }
+}
